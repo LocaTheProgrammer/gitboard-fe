8074f2b61ad88fdd3e67612912741f42aac63487
Pietro <pietro.locatelli@si2001.it>
2022-09-16 12:23:02 +0200

    refactor

--- gitboard/src/app/app-routing.module.ts
+++ gitboard/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './src/app/@pages/home/home.component';
-import { LoginComponent } from './src/app/@pages/login/login.component';
-import { PasswordForgottenComponent } from './src/app/@pages/password-forgotten/password-forgotten.component';
-import { SignupComponent } from './src/app/@pages/signup/signup.component';
+import { HomeComponent } from './@pages/home/home.component';
+import { LoginComponent } from './@pages/login/login.component';
+import { PasswordForgottenComponent } from './@pages/password-forgotten/password-forgotten.component';
+import { SignupComponent } from './@pages/signup/signup.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
--- gitboard/src/app/app.module.ts
+++ gitboard/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { NavbarComponent } from './src/app/@components/navbar/navbar.component';
-import { LoginComponent } from './src/app/@pages/login/login.component';
-import { HomeComponent } from './src/app/@pages/home/home.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { SignupComponent } from './src/app/@pages/signup/signup.component';
-import { PasswordForgottenComponent } from './src/app/@pages/password-forgotten/password-forgotten.component';
+import { HomeComponent } from './@pages/home/home.component';
+import { NavbarComponent } from './@components/navbar/navbar.component';
+import { LoginComponent } from './@pages/login/login.component';
+import { PasswordForgottenComponent } from './@pages/password-forgotten/password-forgotten.component';
+import { SignupComponent } from './@pages/signup/signup.component';
 
 @NgModule({
   declarations: [
