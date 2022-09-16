764a2c4049b4c16e6d499bd92909af033c3a5bf2
Pietro <pietro.locatelli@si2001.it>
2022-09-16 12:16:12 +0200

    password forgotten component

--- gitboard/src/app/src/app/@pages/password-forgotten/password-forgotten.component.ts
+++ gitboard/src/app/src/app/@pages/password-forgotten/password-forgotten.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../@services/user.service';
 
 @Component({
   selector: 'app-password-forgotten',
   templateUrl: './password-forgotten.component.html',
   styleUrls: ['./password-forgotten.component.scss']
 })
-export class PasswordForgottenComponent implements OnInit {
+export class PasswordForgottenComponent{
 
-  constructor() { }
+  email:string="";
+  isMessageSent=0;
 
-  ngOnInit(): void {
+  constructor(private userService:UserService) {}
+
+  resetPassword(){
+    this.userService.forgotPassword(this.email).subscribe(()=>{
+      this.isMessageSent=1
+    },
+    ()=>{
+      this.isMessageSent=2
+    })
   }
 
 }
