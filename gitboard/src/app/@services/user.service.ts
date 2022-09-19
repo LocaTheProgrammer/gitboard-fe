b00334a5eec6e3b42ffa694cc05f8fc9b1fa57ed
Pietro <pietro.locatelli@si2001.it>
2022-09-19 11:16:42 +0200

    refactor user service

--- gitboard/src/app/@services/user.service.ts
+++ gitboard/src/app/@services/user.service.ts
@@ -6,35 +6,35 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UserService {
 
-  auth:string="http://localhost:8090/todolist/";
+  endpoint:string="http://localhost:8090/todolist/";
 
 
   constructor(private HttpClient: HttpClient) { }
 
 
   getBearerToken(username: any, password: any): Observable<any> {
-    return this.HttpClient.post<any>(`${this.auth+'authenticate'}`, { username, password })
+    return this.HttpClient.post<any>(`${this.endpoint+'authenticate'}`, { username, password })
   }
 
   confirmAccount(token:string){
-    return this.HttpClient.get<any>(`${this.auth+'rest/user/confirm-account?token='+token}`)
+    return this.HttpClient.get<any>(`${this.endpoint+'rest/user/confirm-account?token='+token}`)
   }
 
   forgotPassword(email:string){
-    return this.HttpClient.post<any>(`${this.auth+'rest/user/forgot-password'}`, {email})
+    return this.HttpClient.post<any>(`${this.endpoint+'rest/user/forgot-password'}`, {email})
   }
 
   confirmResetPasswordToken(token:string){
-    return this.HttpClient.get<any>(`${this.auth+'rest/user/confirm-reset?token='+token}`)
+    return this.HttpClient.get<any>(`${this.endpoint+'rest/user/confirm-reset?token='+token}`)
   }
 
   resetPassword(email:string, password:string){
-    return this.HttpClient.post<any>(`${this.auth+'rest/user/reset-password'}`, { email, password })
+    return this.HttpClient.post<any>(`${this.endpoint+'rest/user/reset-password'}`, { email, password })
 
   }
 
   createAccount(name:string, surname:string, email:string, password:string){
-    let url=this.auth.concat('rest/user/create')
+    let url=this.endpoint.concat('rest/user/create')
     return this.HttpClient.post<any>(url, {name, surname, email, password})
   }
 }
