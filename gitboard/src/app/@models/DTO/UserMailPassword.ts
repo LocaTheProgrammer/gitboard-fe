bbbf78fff9cebbfec4c062efe52a4cf9237f2958
Pietro <pietro.locatelli@si2001.it>
2022-10-10 09:43:01 +0200

    fixed update password

--- gitboard/src/app/@models/DTO/UserMailPassword.ts
+++ gitboard/src/app/@models/DTO/UserMailPassword.ts
@@ -1,9 +1,11 @@
 export class UserMailPassword {
     public mail: string
     public password: string
+    public oldPassword: string
 
-    constructor(mail: string, password: string) {
+    constructor(mail: string, password: string, oldPassword: string) {
         this.mail = mail
+        this.oldPassword = oldPassword
         this.password = password
     }
 }
